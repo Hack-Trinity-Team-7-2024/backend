a92cc225b1045830c4b74e8300c53c9c4a4b04ac
gmoylan02 <gerardmoylan02@gmail.com>
2024-02-24 15:03:50 +0000

    Added method to regenerate a list of subtasks given user feedback

--- ai_part.py
+++ ai_part.py
@@ -1,7 +1,7 @@
 import dotenv 
 import langchain
 from langchain_openai import ChatOpenAI
-
+import json
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
@@ -39,7 +39,36 @@ def task_breakdown(task: str):
     output = chain.invoke({"input": task})
     return output
 
-if __name__ == '__main__':
 
-    output = task_breakdown("I want to start studying linear algebra, where should I start?")
+def task_recreate_breakdown(task: dict, user_message: str):
+    """
+    Asks the AI to regenerate a list of subtasks, given a list of subtasks and 
+    an explanation as to why they are unsuitable.
+
+    :param task: dict containing the Task name and list of unsuitable sub-tasks
+    :param user_message: User-submitted prompt detailing what about sub-tasks needs to be changed
+    """
+
+    prompt = ChatPromptTemplate.from_messages({
+        ("system", 
+        """You are an expert at breaking down tasks.
+        A Task will be given to you in a JSON format with accompanying Sub-Tasks.
+        The user would like you to re-create the Sub-Tasks for the given Task in JSON
+        giving the existing Task name and the re-created Sub-Tasks
+        and has given feedback.
+        Make sure the JSON formatting is the exact same"""),
+        ("user", "{task_title}"),
+        ("user",  "{subtasks}"),
+        ("user", "{feedback}")
+    })
+    chain = prompt | llm | output_parser
+    output = chain.invoke({"task_title": task["Task"],
+                           "subtasks": [subtask for subtask in task["Sub-Tasks"]],
+                           "feedback": user_message})
+    return output
+
+if __name__ == '__main__':
+    output = task_breakdown("I want learn how to create a RESTful API.")
     print(output)
+    output2 = task_recreate_breakdown(json.loads(output), "Please do it with Express.js")
+    print(output2)
